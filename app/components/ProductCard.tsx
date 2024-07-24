6a85a95eb87c193cc127007ed5dcdbcbc38d7073
Ayush Soni <ayushsoni0718@gmail.com>
2024-07-24 17:00:24 +0530

    learned about client and server components

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+// 'use client';  -> using this the component becomes a client component
+
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
