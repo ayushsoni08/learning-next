c8c41233248db3b7476eafd9083844b9aa0fc169
Ayush Soni <ayushsoni0718@gmail.com>
2024-07-25 15:38:43 +0530

    learned data fetching and caching and static & dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User {
+  // this interface defines the shape of our user object
+  id: number;
+  name: string;
+};
+
+const UsersPage = async () => {
+
+  // Next.js comes with a built-in data cache
+  // Whenever we use the fetch function to get some data, next.js will automatically store the result in the data cache which is based on the file system, so the next we need the same piece of data i.e, the next time we hit the same url to get the data, next.js is not gonna go to the url, its gonna get the data from the data cache from the file system.
+  // Ofcourse, we have full control over this cahcing behaviour
+  // If we have that changes frequently, we can disable caching or treat data in cache as fresh for a certain period of time.
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users",
+    { cache: 'no-store' }   // to disable caching
+    // { next: { revalidate: 10 } }  // to keep data fresh for a certain period of time.   This means that next.js is gonna do a background job and get fresh data from the backend every 10 seconds.
+  );
+
+  // This caching behaviour is only implemented in the fetch function.
+  const users : User[] = await res.json();
+
   return (
-    <div>UsersPage</div>
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   )
 }
 
